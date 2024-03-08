9aa219f28315ce214ce9c1392f6e44baacfbe984
Артур Захаркин <zaharkin.artur75@gmail.com>
2024-03-08 20:14:15 +0700

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Simulation_Lab_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btStart = new System.Windows.Forms.Button();
+            this.edRule = new System.Windows.Forms.NumericUpDown();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.edRule)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 27);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Правило";
+            // 
+            // btStart
+            // 
+            this.btStart.Location = new System.Drawing.Point(440, 19);
+            this.btStart.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(133, 29);
+            this.btStart.TabIndex = 2;
+            this.btStart.Text = "Старт";
+            this.btStart.UseVisualStyleBackColor = true;
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // edRule
+            // 
+            this.edRule.Location = new System.Drawing.Point(77, 25);
+            this.edRule.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.edRule.Maximum = new decimal(new int[] {
+            256,
+            0,
+            0,
+            0});
+            this.edRule.Name = "edRule";
+            this.edRule.Size = new System.Drawing.Size(90, 20);
+            this.edRule.TabIndex = 3;
+            this.edRule.Value = new decimal(new int[] {
+            110,
+            0,
+            0,
+            0});
+            this.edRule.ValueChanged += new System.EventHandler(this.edRule_ValueChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.dataGridView.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.ColumnHeadersVisible = false;
+            this.dataGridView.GridColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.dataGridView.Location = new System.Drawing.Point(25, 58);
+            this.dataGridView.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dataGridView.MultiSelect = false;
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.ReadOnly = true;
+            this.dataGridView.RowHeadersVisible = false;
+            this.dataGridView.RowHeadersWidth = 62;
+            this.dataGridView.RowTemplate.Height = 24;
+            this.dataGridView.Size = new System.Drawing.Size(548, 403);
+            this.dataGridView.TabIndex = 1;
+            this.dataGridView.SelectionChanged += new System.EventHandler(this.dataGridView_SelectionChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(595, 526);
+            this.Controls.Add(this.edRule);
+            this.Controls.Add(this.btStart);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.edRule)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.NumericUpDown edRule;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DataGridView dataGridView;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simulation_Lab_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        const int A = 20;
+
+        private void btStart_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void dataGridView_SelectionChanged(object sender, EventArgs e)
+        {
+            dataGridView.ClearSelection();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int[] rules = new int[8];
+            int rule = (int)edRule.Value;
+            int[,] cells = new int[A, A];
+
+            //перевод правила перехода в двоичную систему и запись в массив
+            string str = Convert.ToString(rule, 2); 
+            int c = Convert.ToInt32(str);
+            for (int j = 7; j >= 0; j--)
+            {
+                rules[j] = c % 10;
+                c = c / 10;
+            }
+
+            // задаем нулевую строку
+            cells[0, 0] = 0; cells[0, 1] = 1; cells[0, 2] = 0; cells[0, 3] = 0; cells[0, 4] = 1; cells[0, 5] = 0; cells[0, 6] = 0; cells[0, 7] = 0; cells[0, 8] = 0; cells[0, 9] = 1; cells[0, 10] = 1;
+            cells[0, 11] = 0; cells[0, 12] = 1; cells[0, 13] = 0; cells[0, 14] = 0; cells[0, 15] = 1; cells[0, 16] = 0; cells[0, 17] = 1; cells[0, 18] = 0; cells[0, 19] = 0;
+
+            //алгоритм при "зацикливании" строки
+            for (int i = 0; i < A - 1; i++) 
+            {
+                int xyz = cells[i, A - 1] * 100 + cells[i, 0] * 10 + cells[i, 1];
+                string str1 = Convert.ToString(xyz);
+                int xyz1 = Convert.ToInt32(str1, 2);
+
+                //перевод по правилу перехода
+                for (int l = 0; l < 8; l++) 
+                {
+                    if (l == xyz1) cells[i + 1, 0] = rules[l];
+                }
+
+                for (int j = 1; j < A - 1; j++)
+                {
+                    xyz = cells[i, j - 1] * 100 + cells[i, j] * 10 + cells[i, j + 1];
+                    str1 = Convert.ToString(xyz);
+                    xyz1 = Convert.ToInt32(str1, 2);
+
+                    //перевод по правилу перехода
+                    for (int l = 0; l < 8; l++) 
+                    {
+                        if (l == xyz1) cells[i + 1, j] = rules[l];
+                    }
+                }
+
+                xyz = cells[i, A - 2] * 100 + cells[i, A - 1] * 10 + cells[i, 0];
+                str1 = Convert.ToString(xyz);
+                xyz1 = Convert.ToInt32(str1, 2);
+
+                //перевод по правилу перехода
+                for (int l = 0; l < 8; l++) 
+                {
+                    if (l == xyz1) cells[i + 1, A - 1] = rules[l];
+                }
+            }
+
+            // Создаем ячейки 
+            dataGridView.ColumnCount = A; 
+            dataGridView.RowCount = A;
+
+            for (int i = 0; i < A; i++)
+            {
+                // раскрашиваем 
+                for (int j = 0; j < A; j++)
+                {
+                    if (cells[i, j] == 1) 
+                        dataGridView.Rows[i].Cells[j].Style.BackColor = Color.Gray;
+                }
+            }
+        }
+
+        private void edRule_ValueChanged(object sender, EventArgs e)
+        {
+            dataGridView.Rows.Clear();
+        }
+    }
+
+}
